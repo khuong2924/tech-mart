468354b86051962fa0e75e33a88193df00ef298f
khuong2924 <truongkhuong666@gmail.com>
2025-05-03 23:07:31 +0700

    init be

---
+++ tech-mart-backend/src/main/java/khuong/com/techmartbackend/TechMartBackendApplication.java
@@ -0,0 +1,13 @@
+package khuong.com.techmartbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechMartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechMartBackendApplication.class, args);
+    }
+
+}
---
+++ tech-mart-backend/src/test/java/khuong/com/techmartbackend/TechMartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package khuong.com.techmartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechMartBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
